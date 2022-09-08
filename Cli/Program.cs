768f6fc5cbf7ecad7271e31bd971a09ad60e416b
Rubens Armôa Lopes <github@rubenslop.es>
2022-09-07 22:26:10 -0400

    feat: add command to know your monster

--- Cli/Program.cs
+++ Cli/Program.cs
@@ -15,12 +15,31 @@ var rootCommand = new RootCommand("True GodMode for GV");
 var issueOption = OptionsFactory.CreateIssueOption();
 rootCommand.AddGlobalOption(issueOption);
 
-rootCommand.SetHandler(async issue =>
-{
-  var crossword = await newspaperReader.GetCrosswordAsync(issue);
+var crosswordCommand = new Command("crossword", "Try to solve the crossword and print the result on screen.");
+crosswordCommand.AddAlias("cw");
+crosswordCommand.SetHandler(SolveCrossword(newspaperReader, godWikiReader), issueOption);
 
-  crossword.Solve((await godWikiReader.GetOmnibusListAsync(issue)).ToArray());
-  Drawer.DrawCrossword(crossword);
-}, issueOption);
+var knowYourMonsterCommand = new Command("know-your-monster", "Try to find out who is the monster of the day.");
+knowYourMonsterCommand.AddAlias("kym");
+knowYourMonsterCommand.SetHandler(() =>
+{
+  Util.WriteWarning("NotImplementedYet");
+});
+
+rootCommand.AddCommand(crosswordCommand);
+rootCommand.AddCommand(knowYourMonsterCommand);
+
+rootCommand.SetHandler(SolveCrossword(newspaperReader, godWikiReader), issueOption);
 
 return await rootCommand.InvokeAsync(args);
+
+Func<string, Task> SolveCrossword(NewspaperReader newspaperReader1, GodWikiReader godWikiReader1)
+{
+  return async issue =>
+  {
+    var crossword = await newspaperReader1.GetCrosswordAsync(issue);
+
+    crossword.Solve((await godWikiReader1.GetOmnibusListAsync(issue)).ToArray());
+    Drawer.DrawCrossword(crossword);
+  };
+}
